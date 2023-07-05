766f176f90629c0fb0393a47f04eef2de598d24e
Tristan Fern <tristan.fern@gmail.com>
2023-07-05 11:21:32 +0200

    added file size management

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,15 +4,16 @@ const router = express.Router();
 
 const auth = require('../middlewares/auth')
 const multer = require('../middlewares/multer.config');
+const sizeChecker = require('../middlewares/sizeChecker')
 
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.getBestRating);
 router.get('/:id', bookCtrl.getBook);
-router.post('/', auth, multer, bookCtrl.createBook);
+router.post('/', auth, sizeChecker, multer, bookCtrl.createBook);
 router.post('/:id/rating', auth, bookCtrl.postRating);
-router.put('/:id', auth, multer, bookCtrl.updateBook);
+router.put('/:id', auth, sizeChecker, multer, bookCtrl.updateBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 
